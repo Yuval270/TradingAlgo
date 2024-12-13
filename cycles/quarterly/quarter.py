4bbfe78e409fad884d686386c1bb78657ca3fe8f
DESKTOP-VO7HNGM\yuval <yuvalat45@gmail.com>
2024-12-13 21:36:22 +0200

    Added create venv

--- cycles/quarterly/quarter.py
+++ cycles/quarterly/quarter.py
@@ -2,8 +2,8 @@ from enum import Enum
 
 
 class Quarter(Enum):
-    Q1 = 1
-    Q2 = 2
-    Q3 = 3
-    Q4 = 4
-    Q5 = 5  # Q5 = Time Distortion from daye perspective
+    Q1 = 0
+    Q2 = 1
+    Q3 = 2
+    Q4 = 3
+    Q5 = 4  # Q5 = Time Distortion from daye perspective
